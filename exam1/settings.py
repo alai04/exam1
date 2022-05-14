b458c13d1e1538de8aeee22fea1d0fa196e672b4
alai04 <alai04@gmail.com>
2022-05-14 16:02:06 +0800

    delete SCERET_KEY

--- exam1/settings.py
+++ exam1/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-&u%)rgid#3by+39qfn_a0u7-&k$$%w$vt#aj_2h^+*z*^f(vf&'
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
